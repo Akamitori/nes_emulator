2561c680f29e8cbbf58bb0a42e02f2e26b9a351b
Akamitori <akamitori@tutanota.com>
2022-08-06 14:16:46 +0300

    Cleaned up EOR tests

--- src/tests/eor_tests.rs
+++ src/tests/eor_tests.rs
@@ -1,7 +1,7 @@
 use crate::{tests::test_helper, CPU};
 
 #[test]
-fn test_0x49_eor_immediate_and() {
+fn test_0x49_eor_immediate() {
     let mut cpu = CPU::new();
 
     let a_value = 0x1;
@@ -14,27 +14,29 @@ fn test_0x49_eor_immediate_and() {
 }
 
 #[test]
-fn test_0x49_eor_zero_flag() {
+fn test_0x49_eor_immediate_zero_flag() {
     let mut cpu = CPU::new();
 
-    let value = 0x32;
-    let prep = test_helper::set_a_to_value(value);
-    let immediate_value = value;
+    let a_value = 0x32;
+    let prep = test_helper::set_a_to_value(a_value);
+    let immediate_value = a_value;
 
     cpu.load_and_run(vec![prep[0], prep[1], 0x49, immediate_value, 0x00]);
 
+    assert_eq!(cpu.register_a, a_value ^ immediate_value);
     test_helper::assert_active_zero_flag(&cpu);
 }
 
 #[test]
-fn test_0x49_eor_negative_flag() {
+fn test_0x49_eor_immediate_negative_flag() {
     let mut cpu = CPU::new();
 
-    let value = 0xFF;
-    let prep = test_helper::set_a_to_value(value);
+    let a_value = 0xFF;
+    let prep = test_helper::set_a_to_value(a_value);
     let immediate_value = 0x0;
     cpu.load_and_run(vec![prep[0], prep[1], 0x49, immediate_value, 0x00]);
 
+    assert_eq!(cpu.register_a, a_value ^ immediate_value);
     test_helper::assert_active_negative_flag(&cpu);
 }
 
@@ -42,50 +44,56 @@ fn test_0x49_eor_negative_flag() {
 fn test_0x45_eor_zero_page() {
     let mut cpu = CPU::new();
     let mem_value = 0x1;
+    let mem_pos = 0x10;
 
-    cpu.mem_write(0x10, mem_value);
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
-    cpu.load_and_run(vec![prep[0], prep[1], 0x45, 0x10, 0x00]);
+    cpu.load_and_run(vec![prep[0], prep[1], 0x45, mem_pos as u8, 0x00]);
 
     assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_inactive_zero_negative_flags(&cpu);
 }
 
 #[test]
-fn test_0x45_eor_negative_flag() {
+fn test_0x45_eor_zero_page_zero_flag() {
     let mut cpu = CPU::new();
     let mem_value = 0xF0;
+    let mem_pos = 0x10;
 
-    cpu.mem_write(0x10, mem_value);
-
-    let a_value = 0x01;
-    let prep = test_helper::set_a_to_value(a_value);
-    cpu.load_and_run(vec![prep[0], prep[1], 0x45, 0x10, 0x00]);
-
-    test_helper::assert_active_negative_flag(&cpu);
-}
-
-#[test]
-fn test_0x45_eor_zero_flag() {
-    let mut cpu = CPU::new();
-    let mem_value = 0xF0;
-
-    cpu.mem_write(0x10, mem_value);
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0xF0;
     let prep = test_helper::set_a_to_value(a_value);
-    cpu.load_and_run(vec![prep[0], prep[1], 0x45, 0x10, 0x00]);
+    cpu.load_and_run(vec![prep[0], prep[1], 0x45, mem_pos as u8, 0x00]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_zero_flag(&cpu);
+}
+
+#[test]
+fn test_0x45_eor_zero_page_negative_flag() {
+    let mut cpu = CPU::new();
+    let mem_value = 0xF0;
+    let mem_pos = 0x10;
+
+    cpu.mem_write(mem_pos, mem_value);
+
+    let a_value = 0x01;
+    let prep = test_helper::set_a_to_value(a_value);
+    cpu.load_and_run(vec![prep[0], prep[1], 0x45, mem_pos as u8, 0x00]);
+
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
+    test_helper::assert_active_negative_flag(&cpu);
 }
 
 #[test]
 fn test_0x55_eor_zero_page_x() {
     let mut cpu = CPU::new();
     let mem_value = 0x1;
-    cpu.mem_write(0x10, mem_value);
+    let mem_pos = 0x10;
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -95,7 +103,7 @@ fn test_0x55_eor_zero_page_x() {
         prep[1],
         test_helper::increase_x_by_one(),
         0x55,
-        0x0F,
+        (mem_pos - 1) as u8,
         0x00,
     ]);
 
@@ -104,10 +112,11 @@ fn test_0x55_eor_zero_page_x() {
 }
 
 #[test]
-fn test_0x55_eor_zero_flag() {
+fn test_0x55_eor_zero_page_x_zero_flag() {
     let mut cpu = CPU::new();
     let mem_value = 0x32;
-    cpu.mem_write(0x10, mem_value);
+    let mem_pos = 0x10;
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = mem_value;
     let prep = test_helper::set_a_to_value(a_value);
@@ -117,18 +126,20 @@ fn test_0x55_eor_zero_flag() {
         prep[1],
         test_helper::increase_x_by_one(),
         0x55,
-        0x0F,
+        (mem_pos - 1) as u8,
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, a_value ^ mem_value);
     test_helper::assert_active_zero_flag(&cpu);
 }
 
 #[test]
-fn test_0x55_eor_negative_flag() {
+fn test_0x55_eor_zero_page_x_negative_flag() {
     let mut cpu = CPU::new();
     let mem_value = 0xFF;
-    cpu.mem_write(0x10, mem_value);
+    let mem_pos = 0x10;
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -138,10 +149,11 @@ fn test_0x55_eor_negative_flag() {
         prep[1],
         test_helper::increase_x_by_one(),
         0x55,
-        0x0F,
+        (mem_pos - 1) as u8,
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, a_value ^ mem_value);
     test_helper::assert_active_negative_flag(&cpu);
 }
 
@@ -149,39 +161,68 @@ fn test_0x55_eor_negative_flag() {
 fn test_0x4d_eor_absolute() {
     let mut cpu = CPU::new();
     let mem_value = 0x1;
-    cpu.mem_write(0x1000, mem_value);
+    let mem_pos = 0x1000;
+    let mem_pos_bytes = (mem_pos as u16).to_le_bytes();
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
-    cpu.load_and_run(vec![prep[0], prep[1], 0x4d, 0x00, 0x10, 0x00]);
+    cpu.load_and_run(vec![
+        prep[0],
+        prep[1],
+        0x4d,
+        mem_pos_bytes[0],
+        mem_pos_bytes[1],
+        0x00,
+    ]);
 
     assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_inactive_zero_negative_flags(&cpu);
 }
 
 #[test]
-fn test_0x4d_eor_zero_flag() {
+fn test_0x4d_eor_absolute_zero_flag() {
     let mut cpu = CPU::new();
     let mem_value = 0x1;
-    cpu.mem_write(0x1000, mem_value);
+    let mem_pos = 0x1000;
+    let mem_pos_bytes = (mem_pos as u16).to_le_bytes();
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x1;
     let prep = test_helper::set_a_to_value(a_value);
-    cpu.load_and_run(vec![prep[0], prep[1], 0x4d, 0x00, 0x10, 0x00]);
+    cpu.load_and_run(vec![
+        prep[0],
+        prep[1],
+        0x4d,
+        mem_pos_bytes[0],
+        mem_pos_bytes[1],
+        0x00,
+    ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_zero_flag(&cpu);
 }
 
 #[test]
-fn test_0x4d_eor_negative_flag() {
+fn test_0x4d_eor_absolute_negative_flag() {
     let mut cpu = CPU::new();
     let mem_value = 0xFF;
-    cpu.mem_write(0x1000, mem_value);
+    let mem_pos = 0x1000;
+    let mem_pos_bytes = (mem_pos as u16).to_le_bytes();
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
-    cpu.load_and_run(vec![prep[0], prep[1], 0x4d, 0x00, 0x10, 0x00]);
+    cpu.load_and_run(vec![
+        prep[0],
+        prep[1],
+        0x4d,
+        mem_pos_bytes[0],
+        mem_pos_bytes[1],
+        0x00,
+    ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_negative_flag(&cpu);
 }
 
@@ -189,7 +230,9 @@ fn test_0x4d_eor_negative_flag() {
 fn test_0x5d_eor_absolute_x() {
     let mut cpu = CPU::new();
     let mem_value = 0x1;
-    cpu.mem_write(0x1000, mem_value);
+    let mem_pos = 0x1000;
+    let mem_pos_bytes = (mem_pos - 1 as u16).to_le_bytes();
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -198,8 +241,8 @@ fn test_0x5d_eor_absolute_x() {
         prep[1],
         test_helper::increase_x_by_one(),
         0x5d,
-        0xFF,
-        0x0F,
+        mem_pos_bytes[0],
+        mem_pos_bytes[1],
         0x00,
     ]);
 
@@ -208,10 +251,12 @@ fn test_0x5d_eor_absolute_x() {
 }
 
 #[test]
-fn test_0x5d_eor_zero_flag() {
+fn test_0x5d_eor_absolute_x_zero_flag() {
     let mut cpu = CPU::new();
     let mem_value = 0x1;
-    cpu.mem_write(0x1000, mem_value);
+    let mem_pos = 0x1000;
+    let mem_pos_bytes = (mem_pos - 1 as u16).to_le_bytes();
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = mem_value;
     let prep = test_helper::set_a_to_value(a_value);
@@ -220,19 +265,22 @@ fn test_0x5d_eor_zero_flag() {
         prep[1],
         test_helper::increase_x_by_one(),
         0x5d,
-        0xFF,
-        0x0F,
+        mem_pos_bytes[0],
+        mem_pos_bytes[1],
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_zero_flag(&cpu);
 }
 
 #[test]
-fn test_0x5d_eor_negative_flag() {
+fn test_0x5d_eor_absolute_x_negative_flag() {
     let mut cpu = CPU::new();
     let mem_value = 0xFF;
-    cpu.mem_write(0x1000, mem_value);
+    let mem_pos = 0x1000;
+    let mem_pos_bytes = (mem_pos - 1 as u16).to_le_bytes();
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -241,11 +289,12 @@ fn test_0x5d_eor_negative_flag() {
         prep[1],
         test_helper::increase_x_by_one(),
         0x5d,
-        0xFF,
-        0x0F,
+        mem_pos_bytes[0],
+        mem_pos_bytes[1],
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_negative_flag(&cpu);
 }
 
@@ -253,7 +302,9 @@ fn test_0x5d_eor_negative_flag() {
 fn test_0x59_eor_absolute_y() {
     let mut cpu = CPU::new();
     let mem_value = 0x1;
-    cpu.mem_write(0x1000, mem_value);
+    let mem_pos = 0x1000;
+    let mem_pos_bytes = (mem_pos - 1 as u16).to_le_bytes();
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -262,8 +313,8 @@ fn test_0x59_eor_absolute_y() {
         prep[1],
         test_helper::increase_y_by_one(),
         0x59,
-        0xFF,
-        0x0F,
+        mem_pos_bytes[0],
+        mem_pos_bytes[1],
         0x00,
     ]);
 
@@ -272,10 +323,12 @@ fn test_0x59_eor_absolute_y() {
 }
 
 #[test]
-fn test_0x59_eor_zero_flag() {
+fn test_0x59_eor_absolute_y_zero_flag() {
     let mut cpu = CPU::new();
     let mem_value = 0x1;
-    cpu.mem_write(0x1000, mem_value);
+    let mem_pos = 0x1000;
+    let mem_pos_bytes = (mem_pos - 1 as u16).to_le_bytes();
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = mem_value;
     let prep = test_helper::set_a_to_value(a_value);
@@ -284,19 +337,22 @@ fn test_0x59_eor_zero_flag() {
         prep[1],
         test_helper::increase_y_by_one(),
         0x59,
-        0xFF,
-        0x0F,
+        mem_pos_bytes[0],
+        mem_pos_bytes[1],
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_zero_flag(&cpu);
 }
 
 #[test]
-fn test_0x59_eor_negative_flag() {
+fn test_0x59_eor_absolute_y_negative_flag() {
     let mut cpu = CPU::new();
     let mem_value = 0xF1;
-    cpu.mem_write(0x1000, mem_value);
+    let mem_pos = 0x1000;
+    let mem_pos_bytes = (mem_pos - 1 as u16).to_le_bytes();
+    cpu.mem_write(mem_pos, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -305,11 +361,12 @@ fn test_0x59_eor_negative_flag() {
         prep[1],
         test_helper::increase_y_by_one(),
         0x59,
-        0xFF,
-        0x0F,
+        mem_pos_bytes[0],
+        mem_pos_bytes[1],
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_negative_flag(&cpu);
 }
 
@@ -318,9 +375,10 @@ fn test_0x41_eor_indirect_x() {
     let mut cpu = CPU::new();
     let mem_to_load: u8 = 0x40;
     let mem_value = 0x1;
+    let mem_indirect=0x0001;
 
-    cpu.mem_write_u16(mem_to_load as u16, 0x0001);
-    cpu.mem_write(0x0001, mem_value);
+    cpu.mem_write_u16(mem_to_load as u16, mem_indirect);
+    cpu.mem_write(mem_indirect, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -338,13 +396,14 @@ fn test_0x41_eor_indirect_x() {
 }
 
 #[test]
-fn test_0x41_eor_zero_flag() {
+fn test_0x41_eor_indirect_x_zero_flag() {
     let mut cpu = CPU::new();
     let mem_to_load: u8 = 0x40;
     let mem_value = 0x1;
+    let mem_indirect=0x0001;
 
-    cpu.mem_write_u16(mem_to_load as u16, 0x0001);
-    cpu.mem_write(0x0001, mem_value);
+    cpu.mem_write_u16(mem_to_load as u16, mem_indirect);
+    cpu.mem_write(mem_indirect, mem_value);
 
     let a_value = mem_value;
     let prep = test_helper::set_a_to_value(a_value);
@@ -357,17 +416,19 @@ fn test_0x41_eor_zero_flag() {
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_zero_flag(&cpu);
 }
 
 #[test]
-fn test_0x41_eor_negative_flag() {
+fn test_0x41_eor_indirect_x_negative_flag() {
     let mut cpu = CPU::new();
     let mem_to_load: u8 = 0x40;
     let mem_value = 0xF1;
+    let mem_indirect=0x0001;
 
-    cpu.mem_write_u16(mem_to_load as u16, 0x0001);
-    cpu.mem_write(0x0001, mem_value);
+    cpu.mem_write_u16(mem_to_load as u16, mem_indirect);
+    cpu.mem_write(mem_indirect, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -380,6 +441,7 @@ fn test_0x41_eor_negative_flag() {
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_negative_flag(&cpu);
 }
 
@@ -388,8 +450,10 @@ fn test_0x51_eor_indirect_y() {
     let mut cpu = CPU::new();
     let mem_to_load: u8 = 0x40;
     let mem_value = 0x1;
-    cpu.mem_write_u16(mem_to_load as u16, 0x0010);
-    cpu.mem_write(0x0011, mem_value);
+    let mem_indirect=0x0011;
+
+    cpu.mem_write_u16(mem_to_load as u16, mem_indirect-1);
+    cpu.mem_write(mem_indirect, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -407,12 +471,14 @@ fn test_0x51_eor_indirect_y() {
 }
 
 #[test]
-fn test_0x51_eor_zero_flag() {
+fn test_0x51_eor_indirect_y_zero_flag() {
     let mut cpu = CPU::new();
     let mem_to_load: u8 = 0x40;
     let mem_value = 0x1;
-    cpu.mem_write_u16(mem_to_load as u16, 0x0010);
-    cpu.mem_write(0x0011, mem_value);
+    let mem_indirect=0x0011;
+
+    cpu.mem_write_u16(mem_to_load as u16, mem_indirect-1);
+    cpu.mem_write(mem_indirect, mem_value);
 
     let a_value = mem_value;
     let prep = test_helper::set_a_to_value(a_value);
@@ -425,16 +491,19 @@ fn test_0x51_eor_zero_flag() {
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_zero_flag(&cpu);
 }
 
 #[test]
-fn test_0x51_eor_negative_flag() {
+fn test_0x51_eor_indirect_y_negative_flag() {
     let mut cpu = CPU::new();
     let mem_to_load: u8 = 0x40;
     let mem_value = 0xF1;
-    cpu.mem_write_u16(mem_to_load as u16, 0x0010);
-    cpu.mem_write(0x0011, mem_value);
+    let mem_indirect=0x0011;
+
+    cpu.mem_write_u16(mem_to_load as u16, mem_indirect-1);
+    cpu.mem_write(mem_indirect, mem_value);
 
     let a_value = 0x0;
     let prep = test_helper::set_a_to_value(a_value);
@@ -447,5 +516,6 @@ fn test_0x51_eor_negative_flag() {
         0x00,
     ]);
 
+    assert_eq!(cpu.register_a, mem_value ^ a_value);
     test_helper::assert_active_negative_flag(&cpu);
 }
